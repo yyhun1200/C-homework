f4f470e9f2a6da61137016e8f6d6656e9987adc7
yyhun1200 <yyhun1200@gmail.com>
2020-10-23 11:02:13 +0900

    P#1 requirement1:게임구현 완료(리팩토링 필요)

--- P#3/P#3.c
+++ P#3/P#3.c
@@ -5,11 +5,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void print_board(char array[][7], int num, int turn);
+void print_board(char array[][7], int num, int turn, int win);
 int check_board(char array[][7], int num, int row, int column, int updown, int side);
 
 int main()
 {
+  int count = 0;
   int column, turn = 1;
   char mark[4] = {'_', 'O', '@', 'X'};
   char board[6][7] = {
@@ -22,14 +23,24 @@ int main()
   };
 
   while(true) {
-    print_board(board, 6, turn);
+    print_board(board, 6, turn, 0);
     scanf("%d", &column);
 
     for(int i = 5; i >= 0; i--)
     {
       if(board[i][column-1] == mark[0]) {
         board[i][column-1] = mark[turn];
-        int count = check_board(board, 6, i, column-1, 1, 0); // count를 활용하여 승리조건 
+        for(int j = -1; j < 2; j++)
+        {
+          for(int k = -1; k < 2; k++)
+          {
+            count = check_board(board, 6, i, column-1, j, k); // count를 활용하여 승리조건 
+            if(count == 4) {
+              print_board(board, 6, turn, 1);
+              return 0;
+            }
+          }
+        }
         break;
       }
     }
@@ -37,18 +48,28 @@ int main()
   }
 }
 
-void print_board(char array[][7], int num, int turn)
+void print_board(char array[][7], int num, int turn, int win)
 {
   printf("  1   2   3   4   5   6   7\n");
   for(int i = 0; i < 6; i++)
     printf("|_%c_|_%c_|_%c_|_%c_|_%c_|_%c_|_%c_|\n", array[i][0], array[i][1], array[i][2], array[i][3], array[i][4], array[i][5], array[i][6]);
-  printf("P1's marker is O, P2's marker is @\n");
-  printf("P%d's turn, Enter the column number:", turn);
+  if(win == 0) {
+    printf("P1's marker is O, P2's marker is @\n");
+    printf("P%d's turn, Enter the column number: ", turn);
+  }
+  else
+  printf("P%d wins!", turn);
 }
+
 int check_board(char array[][7], int num, int row, int column, int updown, int side)
-{
-  if(array[row][column] == array[row+updown][column+side])
-        return 1 + check_board(array, num, row+updown, column+side, updown, side);
-    else
-        return 1;
+{ 
+  // 6 > row + updown > 0 || 7 > column + side > 0
+  if(row + updown < 0 || row+updown > 6 || column + side < 0 || column + side>7)
+    return 1;
+  else if(updown == 0 && side == 0)
+    return 1;
+  else if(array[row][column] == array[row+updown][column+side])
+    return 1 + check_board(array, num, row+updown, column+side, updown, side);
+  else
+    return 1;
 }
