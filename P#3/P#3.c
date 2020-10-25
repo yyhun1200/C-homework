d03f10221bfb835f9795fb04f165d797433b68b5
yyhun1200 <yyhun1200@gmail.com>
2020-10-25 19:41:38 +0900

    무승부기능 구현 완료 예외사항 확인 요망

--- P#3/P#3.c
+++ P#3/P#3.c
@@ -10,14 +10,20 @@
 #define BOARDCOLUMN 7
 
 void print_board(char array[][7], int num, int turn, int win);
-int check_win(char board[][7], int num, int row, int column, int turn);
-int check_board(char array[][7], int num, int row, int column, int updown, int side);
+int start_check(char array[][BOARDCOLUMN], int boardnum, int row, int column, int updown, int side);
+int check_start_point(char board[][7], int boardnum, int row, int column, int updown, int side, int *rowstart, int *columnstart);
+int start_check_draw(char array[][BOARDCOLUMN], int boardnum, int row, int column, int updown, int side);
+void check_draw(char board[][7], int boardnum);
 int initialization(char array[][7], int num, char mark[], int num2);
+
 
 int main()
 {
   int count = 0;
   int column, row, turn = 1;
+
+  int start[2] = {0, 0};
+  int *rowstart = &start[0], *columnstart = &start[1];
 
   char mark[4] = {'_', 'O', '@', 'X'};
   char board[BOARDROW][BOARDCOLUMN] = {
@@ -28,82 +34,126 @@ int main()
     {"_______"},
     {"_______"}
   };
-
+  // 심판
   initialization(board, BOARDROW, mark, 4);
 
+  // Game Start
   while(true) {
     print_board(board, BOARDROW, turn, 0);
     scanf("%d", &column);
     column -= 1;
+
     for(row = BOARDROW-1; row >= 0; row--)
     {
-      if(board[row][column] == mark[0]) 
-      {
-        board[row][column] = mark[turn];     
-        check_win(board, BOARDROW, row, column, turn);
+      if(board[row][column] == mark[0]) {
+        board[row][column] = mark[turn];
+        
+        for(int updown = -1; updown <= 1; updown++) {
+          for(int side = -1; side <= 1; side++) {
+            if(updown == 0 && side == 0) 
+              break;
+            count = check_start_point(board, BOARDROW, row, column, updown, side, rowstart, columnstart);
+            if(count == 4) {
+              print_board(board, BOARDROW, turn, 1);
+              exit(0);
+            }        
+          }
+        }
         break;
       }
     }
-
+    check_draw(board, BOARDROW);
     turn = turn == 1 ? 2 : 1;
   }
-}
-
-// 1.row[3]~[0]까지 각 인덱스를 살펴보고 
-// 2.@,O대상으로 자신의 다음 줄이 자신과 같지 않고, 
-// 자신의 마크나 _의 합이 4이상인게 하나라도 있다면, 진행
-// 4이상 없다면 무승부
-
-int check_win(char board[][7], int num, int row, int column, int turn)
-{
-  for(int updown = -1; updown < 2; updown++) 
-    {
-      for(int side = -1; side < 2; side++) 
-      {
-        int count = check_board(board, BOARDROW, row, column, updown, side);
-        if(count == 4) 
-        {
-          print_board(board, BOARDROW, turn, 1);
-          exit(0);
-        } 
-        else if(count == 3) 
-        {
-          count = check_board(board, BOARDROW, row, column, -updown, -side);
-          if(count == 2) 
-          {
-            print_board(board, BOARDROW, turn, 1);
-            exit(0);
-          }
-        }
-      }
-    }
 }
 
 void print_board(char array[][7], int num, int turn, int win)
 {
   printf("  1   2   3   4   5   6   7\n");
   for(int i = 0; i < 6; i++)
+  {
     printf("|_%c_|_%c_|_%c_|_%c_|_%c_|_%c_|_%c_|\n", array[i][0], array[i][1], array[i][2], array[i][3], array[i][4], array[i][5], array[i][6]);
+  }
   if(win == 0) 
   {
     printf("P1's marker is O, P2's marker is @\n");
     printf("P%d's turn, Enter the column number: ", turn);
+  } else if(win == 2){
+    printf("Game ended in a draw.\n");
   }
   else
-    printf("P%d wins!", turn);
+    printf("P%d wins!\n", turn);
 }
 
-int check_board(char array[][7], int num, int row, int column, int updown, int side)
-{ 
-  // 6 > row + updown > 0 || 7 > column + side > 0
-  if(row + updown < 0 || row+updown > 6 || column + side < 0 || column + side>7) // 벽에 닿음
+int start_check(char array[][BOARDCOLUMN], int boardnum, int row, int column, int updown, int side)
+{
+  if(row + updown < 0 || row+updown >= 6 || column + side < 0 || column + side >= 7)
     return 1;
   else if(updown == 0 && side == 0)
     return 1;
   else if(array[row][column] == array[row+updown][column+side])
-    return 1 + check_board(array, num, row+updown, column+side, updown, side);
-  else    
+    return 1 + start_check(array, boardnum, row+updown, column+side, updown, side);
+  else
     return 1;
+}
+
+void check_draw(char board[][7], int boardnum)
+{
+  int max = 0;
+  for(int row = 0; row < BOARDROW; row++)
+  {
+    for(int column= 0; column < BOARDCOLUMN; column++)
+    {
+      for(int updown = -1; updown <= 1; updown++)
+      {
+        for(int side = -1; side <= 1; side++)
+        {
+          max = max < start_check_draw(board, boardnum, row, column, updown, side) ? start_check_draw(board, boardnum, row, column, updown, side) : max;
+        } 
+      }    
+    }
+  }
+  printf("%d\n",max);
+  if(max < 4)
+  {
+    print_board(board, boardnum, 0, 2);
+    exit(0);
+  }
+}
+
+
+int start_check_draw(char array[][BOARDCOLUMN], int boardnum, int row, int column, int updown, int side)
+{  
+    if(array[row][column] == 'X')
+      return 0;
+    else if(row + updown < 0 || row+updown >= 6 || column + side < 0 || column + side >= 7)
+      return 1;
+    else if(updown == 0 && side == 0)
+      return 1;
+    else if(array[row][column] == array[row+updown][column+side] || array[row+updown][column+side] == '_')
+      return 1 + start_check_draw(array, boardnum, row+updown, column+side, updown, side);
+    else
+      return 1;
+}
+
+int check_start_point(char board[][7], int boardnum, int row, int column, int updown, int side, int *rowstart, int *columnstart)
+{ 
+  int count = 0;
+  if(row - updown >= 6 || row - updown < 0 || column - side < 0 || column - side >= 7)
+  {
+    *rowstart = row;
+    *columnstart = column; 
+    count = start_check(board, boardnum, *rowstart, *columnstart, updown, side);    
+  } else if(board[row][column] == board[row-updown][column-side]) {
+    *rowstart = row-updown;
+    *columnstart = column-side;
+    check_start_point(board, boardnum, row-updown, column-side, updown, side, rowstart, columnstart);
+  } else {
+    *rowstart = row;
+    *columnstart = column;    
+    count = start_check(board, boardnum, *rowstart, *columnstart, updown, side);
+    return count;
+  }
 }
 
 int initialization(char array[][7], int num, char mark[], int num2)
@@ -113,20 +163,15 @@ int initialization(char array[][7], int num, char mark[], int num2)
   {
     printf("For initialization, add block: ");
     scanf("%d", &column);
-    if(column == 0) 
-    {
+    if(column == 0) {
       printf("Game started.\n\n");
       return 0;
     }
-    for(int i = 5; i >= 0; i--)
-    {
-      if(array[i][column-1] == mark[0])
-      {
+    for(int i = 5; i >= 0; i--) {
+      if(array[i][column-1] == mark[0]) {
         array[i][column-1] = mark[3];
         break;
       }
     }
   }
 }
-
-
