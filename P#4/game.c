0dd925def6a092155903a74184dd8ee5e4c1281f
yyhun1200 <yyhun1200@gmail.com>
2020-11-13 21:23:39 +0900

    game완성!

--- P#4/game.c
+++ P#4/game.c
@@ -6,13 +6,14 @@
 #define BLOCK_NUM 14
 #define BLOCK_ROW 3
 #define BLOCK_COLUMN 3
-#define BOARD_SIZE 9
+#define BOARD_ROW 9
+#define BOARD_COLUMN 9
 
 // Block
-void makePlayerBlock(int block[][3], int num, char playerBlock[][3], int num1, int turn)
+void makePlayerBlock(int block[][BLOCK_COLUMN], int num, char playerBlock[][BLOCK_COLUMN], int num1, int turn)
 {
-  for(int row = 0; row < 3; row++){
-    for(int column = 0; column < 3; column++){
+  for(int row = 0; row < BLOCK_ROW; row++){
+    for(int column = 0; column < BLOCK_COLUMN; column++){
       if(block[row][column] == 0){
         playerBlock[row][column] = '_';
       } else if(block[row][column] == 1) {
@@ -26,20 +27,25 @@ void makePlayerBlock(int block[][3], int num, char playerBlock[][3], int num1, i
   }
 };
 
-void printBlock(char playerBlock[][3], int num, int turn) {
+void printBlock(char playerBlock[][BLOCK_COLUMN], int num, int turn, int mode) {
   printf("\nP%d's block:\n", turn);
   printf(" _ _ _ \n");
-  for(int i = 0; i< 3; i++){
+  for(int i = 0; i < BLOCK_ROW; i++){
     printf("|");
-    for(int j = 0; j < 3; j++){
+    for(int j = 0; j < BLOCK_COLUMN; j++){
       printf("%c|", playerBlock[i][j]);
     }
     printf("\n");
   }
-  printf("\nPut Your block (r c) or Rotate (0): ");
+  if(mode == 1) {
+    printf("P1 fails to put the block. P2 wins!");
+    exit(0);
+  } else {
+    printf("\nPut Your block (r c) or Rotate (0): ");
+  }
 };
 
-void makeBlock(char playerBlock[][3], int num, int turn) {
+void makeBlock(char playerBlock[][BLOCK_COLUMN], int num, int turn) {
   srand((unsigned int) time(NULL));
   int random = rand() % 14 + 1;
   switch(random) {
@@ -49,7 +55,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,1,1},
         {0,0,0},
         {0,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 2:
@@ -58,7 +64,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,1,1},
         {1,0,0},
         {0,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 3:
@@ -67,7 +73,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,1,1},
         {0,0,1},
         {0,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 4:
@@ -76,7 +82,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,1,1},
         {0,1,0},
         {0,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 5:
@@ -85,7 +91,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {0,1,1},
         {1,1,0},
         {0,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 6:
@@ -94,7 +100,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,1,0},
         {0,1,1},
         {0,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 7:
@@ -103,7 +109,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,1,0},
         {1,1,0},
         {0,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 8:
@@ -112,7 +118,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,0,0},
         {1,1,0},
         {1,1,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 9:
@@ -121,7 +127,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {0,1,1},
         {1,1,0},
         {1,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 10:
@@ -130,7 +136,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,1,0},
         {0,1,1},
         {0,1,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 11:
@@ -139,7 +145,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {0,1,1},
         {1,1,0},
         {0,1,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 12:
@@ -148,7 +154,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {0,0,1},
         {1,1,1},
         {1,0,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 13:
@@ -157,7 +163,7 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {1,0,0},
         {1,1,1},
         {0,0,1}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
     case 14:
@@ -166,36 +172,36 @@ void makeBlock(char playerBlock[][3], int num, int turn) {
         {0,1,0},
         {1,1,1},
         {0,1,0}};
-        makePlayerBlock(block, 3, playerBlock, 3, turn);
+        makePlayerBlock(block, BLOCK_ROW, playerBlock, BLOCK_ROW, turn);
       break;
     }
   }
 };
 
-void rotate(char block[][3], int num) {
+void rotate(char block[][BLOCK_COLUMN], int num) {
   //copy
   char copyBlock[3][3];
-  for(int row = 0; row < 3; row++){
-    for(int column = 0; column < 3; column++){
+  for(int row = 0; row < BLOCK_ROW; row++){
+    for(int column = 0; column < BLOCK_COLUMN; column++){
       copyBlock[row][column] = block[row][column];
     }
   }
 
   // rotate
-  for(int row = 0; row < 3; row++){
-    for(int column = 0; column < 3; column++){
+  for(int row = 0; row < BLOCK_ROW; row++){
+    for(int column = 0; column < BLOCK_COLUMN; column++){
       block[row][column] = copyBlock[2-column][row];
     }
   }
 };
 
 // Board
-void printBoard(char board[][BOARD_SIZE], int num) {
+void printBoard(char board[][BOARD_COLUMN], int num) {
   printf("   1 2 3 4 5 6 7 8 9\n");
   printf("   _ _ _ _ _ _ _ _ _\n");
-  for(int row = 0; row < 9; row++){
+  for(int row = 0; row < BOARD_ROW; row++){
     printf("%d |", row+1);
-    for(int column = 0; column < 9; column++)
+    for(int column = 0; column < BOARD_COLUMN; column++)
     {
       printf("%c|", board[row][column]);
     }
@@ -213,18 +219,18 @@ int userInput(int *rowPointer, int *columnPointer) {
   else // position
   {
     scanf(" %d", columnPointer);
-    // 오류처리 요망(인자 3개 받을때)
     return 2;
   }
 }
 
-int position(char board[][BOARD_SIZE], int num, char playerBlock[][BLOCK_COLUMN], int num1, int row, int column, int turn) {
+// game
+int position(char board[][BOARD_COLUMN], int num, char playerBlock[][BLOCK_COLUMN], int num1, int row, int column, int turn) {
   int error = 0;
   row -= 1;
   column -= 1;
 
-  for(int i = 0; i < 3; i++) {
-    for(int j = 0; j < 3; j++) {      
+  for(int i = 0; i < BLOCK_ROW; i++) {
+    for(int j = 0; j < BLOCK_COLUMN; j++) {
       if(board[row+i][column+j] != '_' && playerBlock[i][j] != '_'){
         error = error + 1;
       }
@@ -232,8 +238,8 @@ int position(char board[][BOARD_SIZE], int num, char playerBlock[][BLOCK_COLUMN]
   }
 
   if(error == 0) {
-    for(int i = 0; i < 3; i++) {
-      for(int j = 0; j < 3; j++) {
+    for(int i = 0; i < BLOCK_ROW; i++) {
+      for(int j = 0; j < BLOCK_COLUMN; j++) {
         if(playerBlock[i][j] != '_')
         {
           board[row+i][column+j] = playerBlock[i][j];
@@ -247,8 +253,57 @@ int position(char board[][BOARD_SIZE], int num, char playerBlock[][BLOCK_COLUMN]
   }
 }
 
+int isWin(char board[][BOARD_COLUMN], int num, char playerBlock[][BLOCK_COLUMN], int num1) {
+  int playerEmptyCount = 0;
+  int boardStartRow, boardStartColumn;
+  int count = 0;
+  bool match = false;
+  char boardSlice[3][3];
+
+  // 원본 보호
+  char copyBlock[3][3];
+  for(int row = 0; row < BLOCK_ROW; row++){
+    for(int column = 0; column < BLOCK_COLUMN; column++){
+      copyBlock[row][column] = playerBlock[row][column];
+      if(playerBlock[row][column] == '_') {
+        playerEmptyCount++;
+      }
+    }
+  }
+  int rotateDegree = 0;
+  while(rotateDegree != 360) {
+    for(boardStartRow = 0; boardStartRow < 7; boardStartRow++) {
+      for(boardStartColumn = 0; boardStartColumn < 7; boardStartColumn++) {
+        for(int row = 0; row < BLOCK_ROW; row++) {
+          for(int column = 0; column < BLOCK_COLUMN; column++) {
+            boardSlice[row][column] = board[boardStartRow+row][boardStartColumn+column];
+          }
+        }
+        count = 0;
+        for(int row = 0; row < BLOCK_ROW; row++) {
+          for(int column = 0; column < BLOCK_COLUMN; column++) {
+            if(copyBlock[row][column] != '_' && boardSlice[row][column] == '_') {
+              count++; // 블록이 들어갈 수 있음 이게 블록크기에서 empty를 뺀 수가 나오면 들어갈 수 있다.
+            }
+          }
+        }
+        if((BLOCK_ROW*BLOCK_COLUMN)-playerEmptyCount == count) {
+          match = true;
+        }
+      }
+    }    
+    rotate(copyBlock, BLOCK_ROW);
+    rotateDegree += 90;
+  }
+  if(match == false) {
+    return 1;
+  }
+  return 0;
+}
+
+// main
 int main() {
-  char board[9][9] = {
+  char board[BOARD_ROW][BOARD_COLUMN] = {
       "_________",
       "_________",
       "_________",
@@ -261,28 +316,31 @@ int main() {
   };
   int turn = 1;
   int row, column;
-  char playerBlock[3][3] = {};
+  char playerBlock[BLOCK_ROW][BLOCK_COLUMN] = {};
 
-  bool gameFinish = false;
-
-  while(!gameFinish) {
+  while(true) {    
     int command = 0;
-
-    printBoard(board, BOARD_SIZE);
-    makeBlock(playerBlock, BLOCK_ROW, turn);
-    printBlock(playerBlock, BLOCK_ROW, turn);
+    printBoard(board, BOARD_ROW);
+    makeBlock(playerBlock, BLOCK_ROW, turn);    
 
     // win or lose
+    int mode = isWin(board, BOARD_ROW, playerBlock, BLOCK_ROW);
+    printBlock(playerBlock, BLOCK_ROW, turn, mode);
+
 
     while(command != 2) {
       command = userInput(&row, &column);
-      if(command == 1) { // rotate      
+      if(command == 1) { 
+        // rotate block
         rotate(playerBlock, BLOCK_ROW);
-        printBlock(playerBlock, BLOCK_ROW, turn);
-      } else if(command == 2) { // position
-        command = position(board, BOARD_SIZE, playerBlock, BLOCK_ROW, row, column, turn);
+        printBlock(playerBlock, BLOCK_ROW, turn, 0);
+      } else if(command == 2) { 
+        // position block
+        command = position(board, BOARD_ROW, playerBlock, BLOCK_ROW, row, column, turn);
         printf("\n");
-        printBlock(playerBlock, BLOCK_ROW, turn);
+        if(command == 3) {
+          printBlock(playerBlock, BLOCK_ROW, turn, 0);
+        }
       }
     }
 
